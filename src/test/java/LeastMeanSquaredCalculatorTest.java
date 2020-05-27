492f19f0e30cca9ba928ee7b9e958ef4763a0e0f
1-Kelvin <kelvin_sparr@hotmail.de>
2020-05-27 15:56:16 +0200

    added second test;

--- src/test/java/LeastMeanSquaredCalculatorTest.java
+++ src/test/java/LeastMeanSquaredCalculatorTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class LeastMeanSquaredCalculatorTest {
 
     @org.junit.jupiter.api.Test
-    void calculate() {
+    void calculate0() {
         LeastMeanSquaredCalculator leastMeanSquaredCalculator = new LeastMeanSquaredCalculator(2);
         INDArray xvec0 = Nd4j.create(new float[][]{{1}, {0}});
         INDArray xvec1 = Nd4j.create(new float[][]{{1}, {1}});
@@ -19,6 +19,22 @@ class LeastMeanSquaredCalculatorTest {
 
         double k = b_k.getDouble(1);
         double d = b_k.getDouble(0);
-        Assert.isTrue(Math.abs(1 - k) < 0.99 && Math.abs(1-d) < 0.99);
+        Assert.isTrue(Math.abs(1 - k) < 0.01 && Math.abs(1-d) < 0.01);
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculate1() {
+        LeastMeanSquaredCalculator leastMeanSquaredCalculator = new LeastMeanSquaredCalculator(2);
+        INDArray xvec0 = Nd4j.create(new float[][]{{1}, {0}});
+        INDArray xvec1 = Nd4j.create(new float[][]{{1}, {1}});
+        INDArray xvec2 = Nd4j.create(new float[][]{{1}, {2}});
+        INDArray b_k;
+        leastMeanSquaredCalculator.calculate(xvec0, 1);
+        leastMeanSquaredCalculator.calculate(xvec1, 3);
+        b_k = leastMeanSquaredCalculator.calculate(xvec2, 5);
+
+        double k = b_k.getDouble(1);
+        double d = b_k.getDouble(0);
+        Assert.isTrue(Math.abs(2 - k) < 0.01 && Math.abs(1-d) < 0.01);
     }
 }
